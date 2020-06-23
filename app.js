f533b121650ed1018f5fbd54199d10977dbfa774
ilsma <rabhi.smain@gmail.com>
2020-06-23 20:40:48 +0200

    create:js: filter function

--- app.js
+++ app.js
@@ -67,3 +67,17 @@ function clearTasks(e) {
         taskList.removeChild(taskList.firstChild);
     }
 }
+//Filter tasks
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+    document.querySelectorAll('.collection-item').forEach
+    (function (task) {
+        const item = task.firstChild.textContent;
+        if (item.toLowerCase().indexOf(text) != -1) {
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+    });
+
+}
