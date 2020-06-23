bd3934c2e13c93de785004636e0511ef6ac5660f
ilsma <rabhi.smain@gmail.com>
2020-06-23 19:41:32 +0200

    create:js: finish add task function

--- app.js
+++ app.js
@@ -20,5 +20,26 @@ function addTask(e) {
     if (taskInput.value === '') {
         alert('Add a task');
     }
-    e.preventdefault();
+    //Create li element
+    const li = document.createElement('li');
+    //Add class to li
+    li.className = 'collection-item';
+    //Create text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
+    // Create new link element (for delete icon)
+    const link = document.createElement('a');
+    //Add class to link
+    link.className = 'delete-item secondary-content';
+    //Add icon html
+    link.innerHTML = 
+    `<i class="fa fa-times" aria-hidden="true"></i>
+    `;
+    //Append the link to li
+    li.appendChild(link);
+    //Append li to ul
+    taskList.appendChild(li);
+   //Clear input
+   taskInput.value = '';
+
+    e.preventDefault();
 }
