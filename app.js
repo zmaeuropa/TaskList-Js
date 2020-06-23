c39638ad58da8f8cf00b3a14068d30a2650bbbb7
ilsma <rabhi.smain@gmail.com>
2020-06-23 19:07:15 +0200

    create: js: loadeventlistener , create addtask function with alert if empty

--- app.js
+++ app.js
@@ -4,3 +4,21 @@ const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
+
+//Load all event listensers
+loadEventListeners();
+
+//Load all event listeners
+function loadEventListeners() {
+    //Add task event
+    form.addEventListener('submit', addTask);
+}
+
+//Add task
+function addTask(e) {
+    
+    if (taskInput.value === '') {
+        alert('Add a task');
+    }
+    e.preventdefault();
+}
